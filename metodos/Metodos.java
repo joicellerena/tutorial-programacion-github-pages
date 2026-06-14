1e8bb25da24c2131a583055ed7653b2d5e4c884f
joicellerena <joicellerena9@gmail.com>
2026-06-13 19:41:03 -0500

    Create Metodos.java

---
+++ metodos/Metodos.java
@@ -0,0 +1,13 @@
+public class Metodos {
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(suma(5,3));
+
+    }
+
+}
