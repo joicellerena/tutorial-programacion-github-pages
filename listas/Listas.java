2dd4f069245691b8def2ec4bbaea48414f138e4f
joicellerena <joicellerena9@gmail.com>
2026-06-13 22:36:55 -0500

    Create Listas.java to manage a list of names

---
+++ listas/Listas.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Listas {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> nombres = new ArrayList<>();
+
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Pedro");
+
+        System.out.println(nombres);
+
+    }
+
+}
